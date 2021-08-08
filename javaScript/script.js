9b18dce3ab60edb6b586f3a5c98b257980ca32bf
Omar Al-Dayyat <jakezooz999@gmail.com>
2021-08-08 14:12:09 +0300

    Desktop layout finish

---
+++ javaScript/script.js
@@ -0,0 +1,8 @@
+// const arrow = document.querySelector(".arrow");
+// const dropDown = document.querySelector(".drop-down");
+// const hea = document.querySelector(".dropDown-question");
+
+// dropDown.addEventListener('click', () => {
+//   arrow.classList.toggle("show");
+//   hea.classList.toggle("shown");
+// })
