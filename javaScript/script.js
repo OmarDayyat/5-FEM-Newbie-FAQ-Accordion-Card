30eadc10f206e9e22590d2408632a3015069db00
Omar Al-Dayyat <jakezooz999@gmail.com>
2021-08-09 11:54:01 +0300

    Working with JS

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,17 +1,20 @@
-const arrow = document.querySelector(".arrow");
-const dropDown = document.querySelector(".drop-down");
-const hea = document.querySelector(".dropDown-question");
-const para = document.querySelector(".para");
+const arrow = document.querySelectorAll(".arrow");
+const dropDown = document.querySelectorAll(".drop-down");
+const hea = document.querySelectorAll('h4');
+const para = document.querySelectorAll("p");
+
+console.log(dropDown)
+
+
+var i;
+var len = dropDown.length;
+
+for (i = 0; i < len; i++) {
+  dropDown[i].addEventListener('click', function () {
+    arrow[i].classList.toggle('show');
+
+  })
+}
 
 
 
-
-dropDown.addEventListener('click', () => {
-  arrow.classList.toggle("show");
-  hea.classList.toggle("shown");
-  para.classList.toggle("hide");
-
-  if (para.style.display === "none") {
-    para.style.display = "block"
-  }
-})
