ceac78b6b0a96a98e5db4e16fad81329c2e20bca
Omar Al-Dayyat <jakezooz999@gmail.com>
2021-08-17 15:32:52 +0300

    Rewriting the whole code using grid

--- javaScript/script.js
+++ javaScript/script.js
@@ -3,18 +3,7 @@ const dropDown = document.querySelectorAll(".drop-down");
 const hea = document.querySelectorAll('h4');
 const para = document.querySelectorAll("p");
 
-console.log(dropDown)
 
-
-var i;
-var len = dropDown.length;
-
-for (i = 0; i < len; i++) {
-  dropDown[i].addEventListener('click', function () {
-    arrow[i].classList.toggle('show');
-
-  })
-}
 
 
 
